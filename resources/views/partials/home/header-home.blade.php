08b57c8b2406a4918419127eca7d8acdb8f3e1b3
Nestordgs <nesor2@gmail.com>
2017-06-26 22:24:23 -0400

    Menu Redes Sociales Header
    Se agrego la funcion para agregar el menu de redes sociales en el header de la pagina web.
    Se agregaron las clases pad-top-15 y pad-btn-15.
    Se agregaron las clases que extienden de font-awesome para los menu de redes sociales.
    Se agregaron los estilos de background para los menus de redes sociales.

--- resources/views/partials/home/header-home.blade.php
+++ resources/views/partials/home/header-home.blade.php
@@ -1,7 +1,7 @@
 {{-- Created by Nestor on 6/7/2017. --}}
-<div class="bg-picture" style="background-image: url('{{ (get_theme_mod('background_image')) ? esc_url(get_theme_mod('background_image')) : '' }}')" >
-    <div class="container">
-        <div class="row pad-15">
+<div class="bg-picture" style="background-image: url('{{ (get_theme_mod('background_image')) ? esc_url(get_theme_mod('background_image')) : '' }}')">
+    <div class="container pad-top-15">
+        <div class="row pad-top-15 pad-btn-15">
             <div class="col-xs-6 col-md-5">
                 <div class="center-table">
                     @if(get_theme_mod('title_site'))
@@ -14,37 +14,17 @@
                     <img src="{{ esc_url(get_theme_mod('logo_header')) }}" alt="{{ esc_attr(get_bloginfo('name')) }}" class="img-responsive center-block">
                 @endif
                 <div class="row">
-                    <div class="col-xs-2 col-xs-offset-2 text-center">
-                        <a href="#">
-                            <span class="fa-stack fa-2x">
-                                <i class="fa fa-circle fa-stack-2x"></i>
-                                <i class="fa fa-instagram fa-stack-1x fa-inverse" aria-hidden="true"></i>
-                            </span>
-                        </a>
-                    </div>
-                    <div class="col-xs-2 text-center">
-                        <a href="#">
-                            <span class="fa-stack fa-2x">
-                                <i class="fa fa-circle fa-stack-2x"></i>
-                                <i class="fa fa-facebook fa-stack-1x fa-inverse" aria-hidden="true"></i>
-                            </span>
-                        </a>
-                    </div>
-                    <div class="col-xs-2 text-center">
-                        <a href="#">
-                            <span class="fa-stack fa-2x">
-                                <i class="fa fa-circle fa-stack-2x"></i>
-                                <i class="fa fa-twitter fa-stack-1x fa-inverse" aria-hidden="true"></i>
-                            </span>
-                        </a>
-                    </div>
-                    <div class="col-xs-2 text-center">
-                        <a href="#">
-                            <span class="fa-stack fa-2x">
-                                <i class="fa fa-circle fa-stack-2x"></i>
-                                <i class="fa fa-pinterest-p fa-stack-1x fa-inverse" aria-hidden="true"></i>
-                            </span>
-                        </a>
+                    <div class="col-xs-10 col-xs-offset-1 text-center">
+                        @php
+                            wp_nav_menu( array(
+                                'theme_location' => 'social_header',
+                                'container'      => 'ul',
+                                'menu_class'     => 'list-inline social text-center sng',
+                                'menu_id'     => 'menu-social-header',
+                                'link_before' => '<span class="fa-stack fa-rest fa-gradient fa-radius"><i class="fa fa-stack-1x text-gray"></i></span>'
+
+                            ));
+                        @endphp
                     </div>
                 </div>
                 @if(get_theme_mod('username'))
